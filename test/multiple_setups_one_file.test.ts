217cfb7fa524c3f7740bcb5e8717ab080907bf85
Tom Aratyn <36707+tomaratyn@users.noreply.github.com>
2023-11-22 14:48:01 -0500

    Not even a single bfore each will work.

--- test/multiple_setups_one_file.test.ts
+++ test/multiple_setups_one_file.test.ts
@@ -1,19 +1,16 @@
 import {beforeEach, describe, expect, it} from "vitest"
 import {fetch, setup} from "@nuxt/test-utils"
+beforeEach(async () => {
+    await setup({browser: false, server: true})
+})
 
 describe('/api/endpoint1', async () => {
-    beforeEach(async () => {
-        await setup({browser: false, server: true})
-    })
     it('should return 404', async () => {
         const r = await fetch('/api/endpoint1')
         expect(r.status).toBe(404)
     })
 })
 describe('/api/endpoint2', async () => {
-    beforeEach(async () => {
-        await setup({browser: false, server: true})
-    })
     it('should return 404', async () => {
         const r = await fetch('/api/endpoint2')
         expect(r.status).toBe(404)
