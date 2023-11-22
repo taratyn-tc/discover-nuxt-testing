bfc64d8a57f2b3e70e75df5d35c8eb46e23e53e0
Tom Aratyn <36707+tomaratyn@users.noreply.github.com>
2023-11-22 11:28:53 -0500

    Demonstrate problem of multiple setup() calls in one file.

---
+++ test/multiple_setups_one_file.test.ts
@@ -0,0 +1,17 @@
+import {describe, expect, it} from "vitest"
+import {fetch, setup} from "@nuxt/test-utils"
+
+describe('/api/endpoint1', async () => {
+    await setup({browser: false, server: true})
+    it('should return 404', async () => {
+        const r = await fetch('/api/endpoint1')
+        expect(r.status).toBe(404)
+    })
+})
+describe('/api/endpoint2', async () => {
+    await setup({browser: false, server: true})
+    it('should return 404', async () => {
+        const r = await fetch('/api/endpoint2')
+        expect(r.status).toBe(404)
+    })
+})
