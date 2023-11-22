4efd53e9b6d8cf453382f5734106b30449b59a58
Tom Aratyn <36707+tomaratyn@users.noreply.github.com>
2023-11-22 14:55:34 -0500

    top level describe also won't work.

--- test/multiple_setups_one_file.test.ts
+++ test/multiple_setups_one_file.test.ts
@@ -1,17 +1,20 @@
-import {describe, expect, it} from "vitest"
+import {beforeEach, describe, expect, it} from "vitest"
 import {fetch, setup} from "@nuxt/test-utils"
 
-describe('/api/endpoint1', async () => {
-    await setup({browser: false, server: true})
-    it('should return 404', async () => {
-        const r = await fetch('/api/endpoint1')
-        expect(r.status).toBe(404)
+describe('top level', () => {
+    beforeEach(async () => {
+        await setup({browser: false, server: true})
     })
-})
-describe('/api/endpoint2', async () => {
-    await setup({browser: false, server: true})
-    it('should return 404', async () => {
-        const r = await fetch('/api/endpoint2')
-        expect(r.status).toBe(404)
+    describe('/api/endpoint1', async () => {
+        it('should return 404', async () => {
+            const r = await fetch('/api/endpoint1')
+            expect(r.status).toBe(404)
+        })
+    })
+    describe('/api/endpoint2', async () => {
+        it('should return 404', async () => {
+            const r = await fetch('/api/endpoint2')
+            expect(r.status).toBe(404)
+        })
     })
 })
